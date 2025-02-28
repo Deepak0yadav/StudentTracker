a50d371e5c9d9be24e3c26927a08fe7d29765320
Deepak0yadav <deepak311n@gmail.com>
2025-02-28 13:42:50 +0530

    updated app.js

--- server/app.js
+++ server/app.js
@@ -2,39 +2,30 @@ import express from "express";
 import cors from "cors";
 import connectDB from "../server/db/connect.js";
 import students from "../server/routes/students.js";
-import "dotenv/config"; 
+import "dotenv/config";
 
-const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL, 
-    credentials: true, 
+    origin: process.env.CLIENT_URL,
+    credentials: true,
   })
 );
 
 app.use(express.json());
 
-// Routes
+
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
 
 app.use("/api/v1/students", students);
 
-// Start the server
-const startServer = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    console.log("Connected to MongoDB");
 
-    app.listen(port, () => {
-      console.log(`Server is running on http://localhost:${port}`);
-    });
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err.message);
-  }
-};
+connectDB(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err.message));
 
-startServer();
+
+export default app;
